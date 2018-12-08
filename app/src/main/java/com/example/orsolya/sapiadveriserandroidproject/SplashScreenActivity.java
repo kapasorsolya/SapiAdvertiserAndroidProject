cc8fb95c745a4e1e6434f4f52e909dd83e23c3cf
Kapas Orsolya <kapas.orsolya@yahoo.com>
2018-12-08 15:05:25 +0200

    Added SplashScreen.

---
+++ app/src/main/java/com/example/orsolya/sapiadveriserandroidproject/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.orsolya.sapiadveriserandroidproject;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static boolean splashLoaded = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!splashLoaded) {
+            setContentView(R.layout.activity_splash_screen);
+            int secondsDelayed = 1;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 5000);
+
+            splashLoaded = true;
+        }
+        else {
+            Intent goToMainActivity = new Intent(SplashScreenActivity.this, MainActivity.class);
+            goToMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(goToMainActivity);
+            finish();
+        }
+    }
+}

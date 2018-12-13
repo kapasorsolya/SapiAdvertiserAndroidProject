fe7b23b4f1c0113c52d395216b6eb1f91c0b1c39
Kapas Orsolya <kapas.orsolya@yahoo.com>
2018-12-13 16:01:47 +0200

    Refactoring Add Advertisements

--- app/src/main/java/com/example/orsolya/sapiadveriserandroidproject/MainActivity.java
+++ app/src/main/java/com/example/orsolya/sapiadveriserandroidproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.orsolya.sapiadveriserandroidproject;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -67,7 +68,10 @@ public class MainActivity extends AppCompatActivity  implements FragmentChangeOn
         fragmentTransaction.commit();
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+    }
 
     private void setImage(Advertisement ad){
         Log.d(TAG, "setImage: setting te image and name to widgets.");

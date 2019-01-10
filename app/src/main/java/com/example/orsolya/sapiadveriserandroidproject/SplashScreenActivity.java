6ff2f3e92236b81f9e860093810004d3e4959cc2
Kapas Orsolya <kapas.orsolya@yahoo.com>
2019-01-10 13:21:26 +0200

    Added animation to splashScreen

--- app/src/main/java/com/example/orsolya/sapiadveriserandroidproject/SplashScreenActivity.java
+++ app/src/main/java/com/example/orsolya/sapiadveriserandroidproject/SplashScreenActivity.java
@@ -5,9 +5,11 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -18,8 +20,11 @@ public class SplashScreenActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         if (!splashLoaded) {
             setContentView(R.layout.activity_splash_screen);
+            View v = findViewById( R.id.sapiImage );
+            startFadeInAnimation(v) ;
             int secondsDelayed = 1;
             new Handler().postDelayed(new Runnable() {
                 public void run() {
@@ -36,5 +41,11 @@ public class SplashScreenActivity extends AppCompatActivity {
             startActivity(goToMainActivity);
             finish();
         }
+    }
+
+    public void startFadeInAnimation(View view) {
+        ImageView imageView = (ImageView) findViewById(R.id.sapiImage);
+        Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_animation);
+        imageView.startAnimation(startAnimation);
     }
 }
